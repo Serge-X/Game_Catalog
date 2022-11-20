ed1466f2ff52804ce0fb5b191a886caadadb06e2
Serge-X <lionheart.sergex@gmail.com>
2022-11-19 21:22:16 -0800

    Updates

---
+++ precache-manifest.fe0691cb2d5a814f25438cf6c3f8a269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261ad0ee0b3ee42b7018c2bac0a0ae20",
+    "url": "/Game_Catalog/index.html"
+  },
+  {
+    "revision": "cbebcee57b9dcfc10d33",
+    "url": "/Game_Catalog/static/css/main.01ecb210.chunk.css"
+  },
+  {
+    "revision": "294e885abd960048e6d8",
+    "url": "/Game_Catalog/static/js/2.4977ecb2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Game_Catalog/static/js/2.4977ecb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbebcee57b9dcfc10d33",
+    "url": "/Game_Catalog/static/js/main.16d23d1b.chunk.js"
+  },
+  {
+    "revision": "e1f57596e1e812fa2de7",
+    "url": "/Game_Catalog/static/js/runtime-main.b31a7869.js"
+  }
+]);
